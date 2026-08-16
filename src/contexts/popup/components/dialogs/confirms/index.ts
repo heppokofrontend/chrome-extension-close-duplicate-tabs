c6e5c714927073e24a93b48ff5346fd8c380da31
heppokofrontend <heppokofrontend@gmail.com>
2026-08-17 00:23:55 +0900

    Rename show-range-modal to show-categorize-modal

--- src/contexts/popup/components/dialogs/confirms/index.ts
+++ src/contexts/popup/components/dialogs/confirms/index.ts
@@ -3,7 +3,7 @@ import { getMessage } from '@/utils';
 
 export * from './show-choices-modal';
 export * from './show-confirm-modal';
-export * from './show-range-modal';
+export * from './show-categorize-modal';
 export * from './show-select-modal';
 
 POPUP_UI.confirmModal.ariaLabel = getMessage('dialog_confirm');
